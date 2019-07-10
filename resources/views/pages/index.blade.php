a30bcf70de1cdb3eb312621e0e752739fd7709c3
cargamoni <ahmetcanirdem@hotmail.com>
2019-07-10 23:21:03 +0300

    Main Buttons

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,6 +7,11 @@
         {{-- Artık Pages Controller ile Alabiliyoruz //$ işaretini unutma --}}
         <h1>{{$title}}</h1>
         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo inventore quos debitis deleniti minus voluptate aperiam nihil, unde iusto quis aliquam illum eos natus doloribus id odio esse asperiores quaerat! </p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">{{ __('Login') }}</a>
+            @if (Route::has('register')) 
+                <a class="btn btn-success btn-lg" href="{{ route('register') }}" role="button">{{ __('Register') }}</a>
+            @endif
+        </p>
     </div>
 @endsection
